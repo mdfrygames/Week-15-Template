96783abbf358a1ffbd9c6bf6a4fb7c902c109aae
mdfrygames <105132477+mdfrygames@users.noreply.github.com>
2024-12-08 21:43:09 -0500

    Added int template.

--- Week 15 Template/Source.cpp
+++ Week 15 Template/Source.cpp
@@ -2,17 +2,29 @@
 
 using namespace std;
 
+
+
 template<typename T>
 T half(T);
 
-template<int>
-int half(int);
+
+template<typename int>
+int half(int num) {
+	return round(static_cast<float>(num) / 2);
+}
+
 
 int main() {
+
 	return 0;
 }
 
 template<typename T>
 T half(T num) {
 	return num / 2;
+}
+
+template<int>
+int half(int num) {
+	return round(static_cast<float>(num) / 2);
 }
