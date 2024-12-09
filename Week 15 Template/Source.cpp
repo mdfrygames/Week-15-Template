aa7ff90290cf12fff81716c45576af5ed18892e1
mdfrygames <105132477+mdfrygames@users.noreply.github.com>
2024-12-08 21:30:07 -0500

    Added function template for floats and doubles.

--- Week 15 Template/Source.cpp
+++ Week 15 Template/Source.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+template<typename T>
+T half(T);
+
+template<int>
+int half(int);
+
 int main() {
 	return 0;
+}
+
+template<typename T>
+T half(T num) {
+	return num / 2;
 }
